a98dedd69af7a6afc3be10af0dfe287e610a0a8c
Sajidur Rahman <sajidur.inbox@gmail.com>
2022-02-08 15:02:41 +0600

    controllers: crud controllers work here

--- studymate/base/views.py
+++ studymate/base/views.py
@@ -1,24 +1,55 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .models import Room
+from .forms import RoomForm
 
 # Create your views here.
 
-rooms = [
-    {'id': 1, 'name': 'Python'},
-    {'id': 2, 'name': 'Flask'},
-    {'id': 3, 'name': 'DJango'}
-]
+# rooms = [
+#     {'id': 1, 'name': 'Python'},
+#     {'id': 2, 'name': 'Flask'},
+#     {'id': 3, 'name': 'DJango'}
+# ]
 
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'home.html', context)
 
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'room.html', context)
+
+
+def create_room(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form': form}
+    return render(request, 'room_form.html', context)
+
+
+def update_room(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'room_form.html', context)
+
+
+def delete_room(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'delete.html', {'obj': room})

f206c881d11b9d4f8d4516096993c085baccc034
sajidurshajib <sajidur.inbox@gmail.com>
2022-10-18 13:01:11 +0600

    view: username

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -19,7 +19,7 @@ def loginPage(request):
         return redirect('home')
 
     if request.method == 'POST':
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
 
         try:
@@ -43,6 +43,18 @@ def loginPage(request):
 def registerPage(request):
     page = 'register'
     form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'An error occured during registration')
+
     context = {'page': page, 'form': form}
     return render(request, 'base/login_register.html', context)
 
